c8a239e9903280b3feb9036dc8df2e12508e81cc
ITITIU20311 <ITITIU20311@student.hcmiu.edu.vn>
2021-11-22 17:14:08 +0700

    1

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -12,11 +12,19 @@ Ex:
 #include <math.h>
 #include <string.h>
 
+int sosanh(const char *s1, const char *s2)
+{
+    for ( ; *s1 == *s2; s1++, s2++)
+	if (*s1 == '\0')
+	    return 0;
+    return ((*(unsigned char *)s1 < *(unsigned char *)s2) ? -1 : +1);
+}
+
 void Ex2(int n, char *str[]){
 	//Your codes here
 	for(int i=0;i<=n-2;i++){
 		for(int j=i+1;j<=n-1;j++){
-			int xet=strcmp(str[i],str[j]);
+			int xet=sosanh(str[i],str[j]);
 			if(xet==-1){
 				char x[]="aaaaaaaaaaa";
 				strcpy(x,str[i]);
